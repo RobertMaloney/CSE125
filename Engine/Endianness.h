2e9b217c6f64205c171da9c5eefa98ba0e68b5b8
kparkins <kjp.parkinson@gmail.com>
2015-04-04 17:53:18 -0700

    format

--- Engine/Endianness.h
+++ Engine/Endianness.h
@@ -2,26 +2,23 @@
 #define GV_ENDIANNESS_H
 
 
+inline short HostToNet(short bytes);
+inline int HostToNet(int bytes);
+inline long HostToNet(long bytes);
+inline float HostToNet(float bytes);
+inline double HostToNet(double bytes);
 
-     inline short HostToNet(short bytes);
-     inline int HostToNet(int bytes);
-     inline long HostToNet(long bytes);
-     inline float HostToNet(float bytes);
-     inline double HostToNet(double bytes);
-    
-     inline short NetToHost(short bytes);
-     inline int NetToHost(int bytes);
-     inline long NetToHost(long bytes);
-     inline float NetToHost(float bytes);
-     inline double NetToHost(double bytes);
+inline short NetToHost(short bytes);
+inline int NetToHost(int bytes);
+inline long NetToHost(long bytes);
+inline float NetToHost(float bytes);
+inline double NetToHost(double bytes);
 
-     inline void ByteSwap(char* first, unsigned int size);
-     inline bool IsBigEndian();
-
+inline void ByteSwap(char* first, unsigned int size);
+inline bool IsBigEndian();
 
 
 #include "Endianness.inl"
-
 #endif
 
 
