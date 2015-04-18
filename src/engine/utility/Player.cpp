5d9f14feb0e883806cbe83f0528c815f0bb0c43a
Beth Shi <bethyus017@gmail.com>
2015-04-17 17:26:16 -0700

    Player class, Location class, Map and World...

---
+++ src/engine/utility/Player.cpp
@@ -0,0 +1,73 @@
+#include "Player.h"
+#include "Location.h"
+
+int numOfPlayers = 0;
+
+//TODO Config file
+Player::Player() {
+	numOfPlayers++;
+	this->pid = numOfPlayers;
+	this->id = "Player" + this->pid;
+	this->speed = 0;
+	this->power = 0;
+	this->loc = Location(0, 0, 0);
+}
+
+int Player::getSpeed() {
+	return speed;
+}
+
+int Player::getPower() {
+	return power;
+}
+
+int Player::getPid() {
+	return pid;
+}
+
+string Player::getId() {
+	return id;
+}
+
+int Player::getX() {
+	return loc.getX();
+}
+
+int Player::getY() {
+	return loc.getY();
+}
+
+int Player::getZ() {
+	return loc.getZ();
+}
+
+Location Player::getLoc() {
+	return loc;
+}
+
+void Player::setLoc(Location newLoc){
+	loc = newLoc;
+}
+
+
+void Player::setSpeed(int newSpeed) {
+	speed = newSpeed;
+}
+
+void Player::setPower(int newPower) {
+	power = newPower;
+}
+
+
+
+void Player::setX(int newX) {
+	loc.setX(newX);
+}
+
+void Player::setY(int newY) {
+	loc.setY(newY);
+}
+
+void Player::setZ(int newZ) {
+	loc.setZ(newZ);
+}
---
+++ src/engine/utility/World.h
@@ -0,0 +1,15 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+class World {
+
+protected:
+	//a list of players?
+	//a list of resources?
+	//a list of NPCs?
+	//a map of everything?
+	//a map of resources?
+public:
+
+};
+#endif
