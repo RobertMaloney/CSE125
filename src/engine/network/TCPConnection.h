7733cd7ffcfed53a972737b800ae1637b8c6a541
kparkins <kjp.parkinson@gmail.com>
2015-04-17 23:07:53 -0700

    commecnts

--- src/engine/network/TCPConnection.h
+++ src/engine/network/TCPConnection.h
@@ -12,9 +12,9 @@ using std::vector;
 typedef uint8_t byte;
 typedef vector<byte> Packet;
 
-const int DEFAULT_SOCKET_BUFSIZ = 1;
-const int BYTES_IN_HEADER = 4;
-const int FREE_THRESHOLD = 512;
+const int DEFAULT_SOCKET_BUFSIZ = 1;             // the default size of buffers
+const int BYTES_IN_HEADER = 4;                   
+const int FREE_THRESHOLD = 512;                  // min number of avail bytes in buffer before expansion
 const int MAX_SOCKET_BUFSIZ = MAX_PACKET_SIZE;
 
 class TCPConnection : public Socket {
@@ -25,32 +25,53 @@ public:
     TCPConnection(int sockFd, SocketAddress remAddr);
     ~TCPConnection();
 
+    /* Connect to the specified ip and port. If the socket can not connect
+     * an SE_NOCONNECT error will be returned
+     */
     SocketError Connect(const string & ip, const string & port);
 
+    /* Send a single packet. Can return many errors. Check Socket.h SocketError
+     * enum for relevant errors to the situation you are using it in.
+     */
     SocketError Send(const Packet & packet);
+
+    /* Send all the packets in the vector. If one of the packets is larger than
+     * 65503 bytes it will be silently dropped.
+     */
     SocketError Send(const vector<Packet> & packets);
 
+    /* Receive a single packet into the packet buffer.
+     */
     SocketError Receive(Packet & packet);
+
+    /* Get all the packets you can from the internal buffer. Use this if you need
+     * to grab all packets instead of looping on the single receive. This will be
+     * more efficient.
+     */
     SocketError Receive(vector<Packet> & packets);
 
 
 protected:
 
-    SocketError Send();
+    SocketError Send();                   // internally used send and recvs'
     SocketError Receive();
-    uint32_t ReadHeader(const int start);
+    uint32_t ReadHeader(const int start); // read the next packet size
 
-    bool WriteToBuffer(const Packet & packet);
-    bool FillFromBuffer(Packet & packet, unsigned int & pos);
-    void ShiftBuffer(vector<byte> & buffer, unsigned int lastRead);
+    bool WriteToBuffer(const Packet & packet); // write to the send buffer
+    bool FillFromBuffer(Packet & packet, unsigned int & pos); // read from the receive buffer
+    void ShiftBuffer(vector<byte> & buffer, unsigned int lastRead); // shift from lastRead to size 
 
+    // vanilla send and receive wrappers
     inline int Send(byte* buffer, int size);
     inline int Recv(byte* buffer, int size);
 
+    // prints all values in the specified buffer. useful for debugging.
     void PrintBuffer(vector<byte> & buffer, std::string msg);
 
+    // internal buffers for sending and receiving
     vector<byte> sendBuffer;
     vector<byte> receiveBuffer;
+
 };
 
 #endif
