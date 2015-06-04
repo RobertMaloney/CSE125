8982eb4c9e7e676786314b1e752df37830aea546
kparkins <kjp.parkinson@gmail.com>
2015-06-03 21:39:34 -0700

    sound enable

--- src/engine/GameServer.cpp
+++ src/engine/GameServer.cpp
@@ -69,6 +69,7 @@ void GameServer::run() {
 	running = true;
 
 	while (running) {
+		serverLock.lock();
 		start = high_resolution_clock::now();
 
 		if (clients->size() < maxConnections) {
@@ -86,14 +87,16 @@ void GameServer::run() {
 			cerr << "Server loop took long than a frame." << endl;
 			cout << "dustyplanet:-$ ";
 		}
-
+		serverLock.unlock();
 		sleep_for(microseconds(TIME_PER_FRAME - elapsedTime));
 	}
 }
 
 
 void GameServer::stop() {
+	serverLock.lock();
 	running = false;
+	serverLock.unlock();
 }
 
 
@@ -150,12 +153,10 @@ void GameServer::tick() {
 	vector<Packet> updates;
 	
 	if (gameState->isResetting()) {
-		serverLock.lock();
 		ObjectDB & odb = ObjectDB::getInstance();
 		odb.reloadObjects(configFile);
 		odb.getObjectState(updates);
 		gameState->setResetting(false);
-		serverLock.unlock();
 	} else {
 		this->getUpdates(updates);
 	}
