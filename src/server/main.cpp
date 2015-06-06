cc657df49c5054f7b712d382805b762ddb05b229
kparkins <kjp.parkinson@gmail.com>
2015-06-06 03:20:01 -0700

    Update main.cpp

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -12,15 +12,6 @@ void stringToLowerCase(string & string) {
 
 }
 
-void trimNewLine(string & string) {
-	for (int i = string.size() - 1; i >= 0; --i){
-		if ((string[i] >= 'a' && string[i] <= 'z') ||
-		    (string[i] >= 'A' && string[i] <= 'Z')) {
-			string.resize(i + 1);
-			break;
-		}
-	}
-}
 
 void execute(string & command, GameServer* server) {
 	if (command == "reset") {
@@ -40,7 +31,6 @@ int main(int argc, char* argv[]) {
 		cout << "dustyplanet:-$ ";
 		cin >> command;
 		stringToLowerCase(command);
-		trimNewLine(command);
 		if (command == "exit") {
 			server.stop();
 			break;
